e9cd2a3dfa540f219e3937d265395a6785a400c9
Paulo Salvatore <salvatoregames@gmail.com>
2018-11-28 10:11:09 -0200

    Initial commit

---
+++ Assets/Scripts/Zumbi.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zumbi : MonoBehaviour
+{
+	public float velocidade = 0.3f;
+
+	public float delayAndar = 1f;
+
+	Rigidbody rb;
+	Animator animator;
+
+	bool andando = false;
+
+	GameObject jogador;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		animator = GetComponent<Animator>();
+
+		Invoke("Andar", delayAndar);
+
+		jogador = GameObject.Find("Jogador");
+	}
+
+	void Andar()
+	{
+		andando = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (andando)
+		{
+			transform.LookAt(jogador.transform);
+
+			rb.velocity = (jogador.transform.position - transform.position).normalized * velocidade;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Projétil"))
+		{
+			animator.SetTrigger("Morrer");
+			andando = false;
+		}
+	}
+}
